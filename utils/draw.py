335797a8c3296f24548cd3abf354f83b6195291f
LeonardoViotti <leonardoviotti@gmail.com>
2021-01-17 13:14:37 -0500

    Reorganization and removal of deprecated code.

--- utils/draw.py
+++ utils/draw.py
@@ -23,10 +23,11 @@ def draw_boxes(img, bbox, identities=None, offset=(0,0)):
         id = int(identities[i]) if identities is not None else 0    
         color = compute_color_for_labels(id)
         label = '{}{:d}'.format("", id)
-        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
-        cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
-        cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
-        cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
+        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+        cv2.rectangle(img,(x1, y1),(x2,y2),color,1)
+        # cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
+        cv2.rectangle(img,(x1, y1),(x1+t_size[0],y1+t_size[1]+1), color,-1)
+        cv2.putText(img,label,(x1,y1+t_size[1]+1), cv2.FONT_HERSHEY_PLAIN, .75, [255,255,255], 1)
     return img
 
 
