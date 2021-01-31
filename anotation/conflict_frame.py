cedc0545520f94d3367fb692456fffcb66027ebc
LeonardoViotti <leonardoviotti@gmail.com>
2021-01-31 12:51:58 -0500

    Folder reorganization.

--- anotation/conflict_frame.py
+++ anotation/conflict_frame.py
@@ -2,6 +2,7 @@
 
 # Settings
 from utils import *
+from modules.draw import *
 
 import copy as cp
 import pandas as pd
@@ -40,77 +41,36 @@ data['class'] = data['class'].map(class_dict)
 data['cx'] =  round(data['xi'] + (data['xj'] - data['xi'])/2).astype(int)
 data['cy'] =  round(data['yi'] + (data['yj'] - data['yi'])/2).astype(int)
 
-]
+
 #------------------------------------------------------------------------------
 # Select first frame
 cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 _, img_0 = cap.read()
 # stable_show(img_0)
 
+# Grab post conflict frame
+cap.set(cv2.CAP_PROP_POS_FRAMES, 143-1)
+_, img_c = cap.read()
+
+
 #------------------------------------------------------------------------------
-# Static annotations
+# Colors
+
+df_conf = data[data['obj_id'].isin([21,22])]
+df1 = df_conf[df_conf['obj_id'] == 21]
+df2 = df_conf[df_conf['obj_id'] == 22]
 
 
-COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+# Loop over different object ids in the df
+img = cp.deepcopy(img_c)
+for oid in trajectories_df['obj_id'].unique():
+    df_i = trajectories_df[trajectories_df['obj_id'] == oid]
+    # print(df_i.head(5))
+    img = draw_trajectory(img, df_i)
 
 
-# Draw single box function
-def draw_box(org_img, bbox_df, class_id):
-    x1,y1,x2,y2 = bbox_df['xi'], bbox_df['yi'], bbox_df['xj'], bbox_df['yj']
-    # Create another image
-    img = cp.deepcopy(org_img)
-    
-    # Coolor and lable
-    color = (0,255,0)
-    label = class_id
-    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
-    # Boox rectangle
-    cv2.rectangle(img,(x1, y1),(x2,y2),color,1)
-    # Text rectangle
-    cv2.rectangle(img,(x1, y1),(x1+t_size[0],y1+t_size[1]+1), color,-1)
-    cv2.putText(img,label,(x1,y1+t_size[1]+1), cv2.FONT_HERSHEY_PLAIN, .75, [255,255,255], 1)
-    
-    return img
+ishow(img)
 
-# Draw single point
-def draw_centroid(org_img, cent_df):
-    img = cp.deepcopy(org_img)
-    # Anotate
-    cv2.circle(img, (cent_df['cx'], cent_df['cy']), 2, (255, 255, 255), -1)
-    
-    return img
-    
-def draw_trajectory(org_img, trajectory_df):
-    """
-    This draws a trakjectorie line on a frame
-    img -  has to be an image converted into numpy.ndarray
-    trajectory_df 
-    """
-    img = cp.deepcopy(org_img)
-    for p in range(1, len(trajectory_df)):
-        # print(p)
-        cv2.line(img, tuple(trajectory_df.iloc[p-1]), tuple(trajectory_df.iloc[p]), (0, 0, 255), 2)
-    return(img)
-
-
-df = data[data['obj_id'] == 1][['cx', 'cy']]
-foo = cp.deepcopy(img_0)
-
-trajectory_df = df
-
-
-
-for idx, row in df.iterrows():
-    print(df[idx])
-
-tuple(df.iloc[0])
-
-
-# foo = draw_boxes(img_0, data_i)
-
-
-# img_show(img_0)
-img_show(foo)
 
 #------------------------------------------------------------------------------
 # Process video frame by frame
