51a9c96c3a96605db2814b5931868657d46cf0e5
LeonardoViotti <leonardoviotti@gmail.com>
2021-02-09 20:30:41 -0500

    Added hstack_images to anotation/modules/utils.py

--- anotation/modules/utils.py
+++ anotation/modules/utils.py
@@ -28,3 +28,16 @@ def vshow(cap):
     # When everything done, release the capture
     # cap.release()
     cv2.destroyAllWindows() 
+
+def hstack_images(img1, img2):
+    #https://stackoverflow.com/a/24522170/8692138
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
+    
+    #create empty matrix
+    vis = np.zeros((max(h1, h2), w1+w2,3), np.uint8)
+    
+    #combine 2 images
+    vis[:h1, :w1,:3] = img1
+    vis[:h2, w1:w1+w2,:3] = img2
+    return vis
