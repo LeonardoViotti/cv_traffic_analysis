5861ba294e64cc423a7321eb5a57c0957f30a6e2
LeonardoViotti <leonardoviotti@gmail.com>
2021-09-14 16:18:19 -0400

    Minor video annotation edits.

--- data-analysis/video_annotation.py
+++ data-analysis/video_annotation.py
@@ -1,3 +1,9 @@
+#-------------------------------------------------------------------------------------
+# Annotate video with a csv file
+#-------------------------------------------------------------------------------------
+
+# TODO
+# - Cut video with to only show frames contained in df
 
 
 #-------------------------------------------------------------------------------------
@@ -73,48 +79,3 @@ cap.release()
 
 # Closes all the frames
 cv2.destroyAllWindows()
-
-#-------------------------------------------------------------------------------------
-# DRAFT
-
-
-bar = draw_box(frame, foo, 'Person')
-ishow(bar)
-
-# Draw single box function
-def draw_box(org_img, bbox_df, class_id, color_dict = color_dict, imutable = True):
-    x1,y1,x2,y2 = bbox_df['xi'], bbox_df['yi'], bbox_df['xj'], bbox_df['yj']
-    
-    # Create another image
-    if imutable:
-        img = cp.deepcopy(org_img)
-    else:
-        img = org_img
-    # Coolor and lable
-    # color = (0,255,0)
-    color  = color_dict[class_id]
-    label = class_id
-    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
-    # Boox rectangle
-    cv2.rectangle(img,(x1, y1),(x2,y2),color,1)
-    # Text rectangle
-    cv2.rectangle(img,(x1, y1),(x1+t_size[0],y1+t_size[1]+1), color,-1)
-    cv2.putText(img,label,(x1,y1+t_size[1]+1), cv2.FONT_HERSHEY_PLAIN, .75, [255,255,255], 1)
-    
-    if imutable:
-        return img
-
-def draw_boxes(img, df, imutable = False):
-    for index, row in df.iterrows():
-        draw_box(img, row, row['class'], imutable = imutable)
-
-draw_boxes(frame, frame_df)
-
-
-frame_df = df[df['frame'] == idx_frame]
-
-# for index, row in frame_df.iterrows():
-#     draw_box(frame, row, row['class'], imutable = False)
-
-# draw_box(frame, foo, 'Person', imutable = False)
-ishow(frame)
